88b40227b682c6ca741ba5e269697fad0a633e35
stuartalexwhitehead <stuart@springload.co.nz>
2017-05-28 13:57:30 +1200

    Fixing initial flake8 warnings

--- stuart_is/settings/production.py
+++ stuart_is/settings/production.py
@@ -1,12 +1,12 @@
 from __future__ import absolute_import, unicode_literals
 
-from .base import *
+from .base import *  # noqa: F401, F403
 
 
 DEBUG = False
 
 
 try:
-    from .local import *
+    from .local import *  # noqa: F401, F403
 except ImportError:
     pass
