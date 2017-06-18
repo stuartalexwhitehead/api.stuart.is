99dc9c1f14c2b0e63d439a0859e46a7c6766b90f
stuartalexwhitehead <stuart@springload.co.nz>
2017-06-18 12:11:25 +1200

    Get production DB settings from env vars

--- stuart_is/settings/production.py
+++ stuart_is/settings/production.py
@@ -1,9 +1,23 @@
 from __future__ import absolute_import, unicode_literals
 
+import os
+
 from .base import *  # noqa: F401, F403
 
 
 DEBUG = False
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'stuartis',
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+    }
+}
 
 
 try:
